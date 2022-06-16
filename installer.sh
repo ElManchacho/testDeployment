c557c83ec094d8bafe2200403e9840421a6fc5e8
ElManchacho <pld.pro@hotmail.com>
2022-06-16 12:16:20 +0200

    stable version

--- installer.sh
+++ installer.sh
@@ -9,3 +9,9 @@ git clone https://github.com/ElManchacho/testDeployment.git
 mv testDeployment/package.json ./
 
 rm testDeployment/ -r
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+
+apt-get install -y nodejs npm
+
+npm install
